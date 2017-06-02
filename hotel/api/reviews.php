f65832ec36e71c36e0bd760960572cbad450175d
pemre <piskin.emre@gmail.com>
2017-06-02 13:54:54 +0300

    Send empty list by %20 chance

--- hotel/api/reviews.php
+++ hotel/api/reviews.php
@@ -19,20 +19,23 @@ if (mt_rand(1,5) == 1) // Die by %20 chance
   $json->error('Something failed!');
 if (!$id || $id < 0) // === 0 || === null
   $json->error('Param "hotel_id" must be set and positive integer');
+if (mt_rand(1,5) == 1) // Send empty list by %20 chance
+  $sendEmpty = true;
 
 // Create a review object
 $review  = new Review();
 // Create a review array to send
 $reviews = array();
 
-for ($i = 1; $i <= mt_rand(1,5); $i++) {
-  array_push($reviews, array(
-    'name'     => $review->name(),
-    'comment'  => $review->comment(),
-    'positive' => $review->positive()
-    )
-  );
-}
+if (!$sendEmpty)
+  for ($i = 1; $i <= mt_rand(1,5); $i++) {
+    array_push($reviews, array(
+      'name'     => $review->name(),
+      'comment'  => $review->comment(),
+      'positive' => $review->positive()
+      )
+    );
+  }
 
 // Echo the json object
 $json->write($reviews);
