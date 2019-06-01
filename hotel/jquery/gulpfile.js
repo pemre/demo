5cc440d3dc31c5ea97991d4d8a12bc785a0e3c2e
emre <emre.piskin@takeaway.com>
2019-06-01 16:18:35 +0200

    upgraded Gulp to version 4

--- hotel/jquery/gulpfile.js
+++ hotel/jquery/gulpfile.js
@@ -1,53 +1,47 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    connect = require('gulp-connect');
+const { series, parallel } = require('gulp');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const connect = require('gulp-connect');
+const sass = require('gulp-sass');
 
-// connect the server
-gulp.task('connect', function(){
-  connect.server({
+function connectServer () {
+  return connect.server({
     root: 'dist',
     livereload: true
-  });
-});
+  })
+}
 
-// copy only index file to dist folder
-gulp.task('copy-index', function () {
-    return gulp.src(['./index.html'])
-        .pipe(gulp.dest('./dist'))
-});
+function copyIndexToDist () {
+  return gulp.src(['./index.html'])
+    .pipe(gulp.dest('./dist'))
+}
 
-// copy all files to dist folder
-gulp.task('copy-all', function () {
-    return gulp.src([
-        './index.html',
-        '*tpl/**/*',
-        '*img/**/*',
-        '*js/**/*'
-    ])
-        .pipe(gulp.dest('./dist'))
-});
+function copyAllToDist () {
+  return gulp.src([
+    './index.html',
+    '*tpl/**/*',
+    '*img/**/*',
+    '*js/**/*'
+  ])
+    .pipe(gulp.dest('./dist'))
+}
 
-// keeps gulp from crashing for scss errors
-gulp.task('sass', function () {
-  return gulp.src(['../*.scss','./sass/*.scss'])
-      .pipe(sass({ errLogToConsole: true, outputStyle: 'compressed' }))
-      .pipe(concat('style.min.css'))
-      .pipe(gulp.dest('./dist/css'));
-});
+function compileSass () {
+  return gulp.src(['../*.scss', './sass/*.scss'])
+    .pipe(sass({ errLogToConsole: true, outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./dist/css'))
+}
 
-// launch livereload
-gulp.task('livereload', function (){
-  gulp.src('./dist/**/*')
-  .pipe(connect.reload());
-});
+function livereload () {
+  return gulp.src('./dist/**/*')
+    .pipe(connect.reload())
+}
 
-// watch only given files
-gulp.task('watch', function () {
-  gulp.watch('./index.html', ['copy-index']);
-  gulp.watch(['../*.scss','./sass/**/*.scss'], ['sass']);
-  gulp.watch('./dist/**/*', ['livereload']);
-});
+function watch () {
+  gulp.watch('./index.html', copyIndexToDist);
+  gulp.watch(['../*.scss', './sass/**/*.scss'], compileSass);
+  gulp.watch('./dist/**/*', livereload);
+}
 
-// default run script
-gulp.task('default', ['copy-all', 'connect', 'watch', 'sass']);
+exports.default = parallel(copyAllToDist, connectServer, watch, compileSass);
